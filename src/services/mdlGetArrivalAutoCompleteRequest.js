4b6a7d6e9910129a5ec1280545e1cb5f2e3d9e3a
Ömer Süt <omersutcom@gmail.com>
2022-05-11 14:10:47 +0300

    axios request

---
+++ src/services/mdlGetArrivalAutoCompleteRequest.js
@@ -0,0 +1,22 @@
+import axios from "axios"
+
+const mdlGetArrivalAutoCompleteRequest = (location, headers, setResponse, setError, response) => {
+    axios.post('https://preprod-services.tourvisio.com/v2/api/productservice/getarrivalautocomplete', {
+        culture: "en-US",
+        currency: "EUR",
+        productType: 2,
+        query: location
+     }, {
+      header: headers
+    }) // search is the object with the search parameters
+      .then(res => {
+        setResponse(res.data)
+        console.log(response)
+      }
+      )
+      .catch(err => {
+        setError(err.response.data)
+      }
+      )
+} 
+export default mdlGetArrivalAutoCompleteRequest
