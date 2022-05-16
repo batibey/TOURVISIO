2344ebb81d9eea67c8254dc80a9efcdf1ebdca73
Mustafa BATI <mustafa.bati9@gmail.com>
2022-05-16 14:24:40 +0300

    setReservationInfo-eklendi

--- src/services/BookingService/removeServices.js
+++ src/services/BookingService/removeServices.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const mdlRemoveServicesRequest = (setRemoveServicesResponse, setError, getRemoveServicesRequestResponse) => {
+const mdlRemoveServicesRequest = (setRemoveServicesResponse, setError, getRemoveServicesResponse) => {
     axios.post(`${process.env.REACT_APP_API_URL}/bookingservice/removeservices`, {
         transactionId: "fad80a51-3ed0-4502-b623-31992a73aa14",
         services: [
@@ -11,11 +11,11 @@ const mdlRemoveServicesRequest = (setRemoveServicesResponse, setError, getRemove
     })
         .then(res => {
             setRemoveServicesResponse(res.data)
-            console.log(getRemoveServicesRequestResponse)
+            console.log(getRemoveServicesResponse)
         }
         )
         .catch(err => {
-            setError(err.getRemoveServicesRequestResponse.data)
+            setError(err.getRemoveServicesResponse.data)
         }
         )
 }
